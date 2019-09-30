3e1ff735a1064293072f278a6614abee54920168
TUSPT <>
2019-09-30 21:11:34 +0700

    first commit

---
+++ src/main/java/spring/tourbookingbackend/TourbookingBackendApplication.java
@@ -0,0 +1,13 @@
+package spring.tourbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourbookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourbookingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/tourbookingbackend/TourbookingBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package spring.tourbookingbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TourbookingBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
