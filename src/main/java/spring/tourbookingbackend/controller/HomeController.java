123b182635f1c68486ec89138e768c5fa9764368
TUSPT <>
2019-10-01 11:22:03 +0700

    two commit

---
+++ src/main/java/spring/tourbookingbackend/controller/HomeController.java
@@ -0,0 +1,15 @@
+package spring.tourbookingbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Home Controller";
+    }
+}
